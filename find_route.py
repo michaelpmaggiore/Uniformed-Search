eb6d6e61350423fd18a6eadcb2726e06b0e2b25b
Michael Maggiore <michaelpmaggiore@gmail.com>
2023-05-02 23:51:39 -0600

    Fixed bug. Properly loaded in cities to dictioanry

--- find_route.py
+++ find_route.py
@@ -3,7 +3,8 @@ import sys
 input_filename = sys.argv[1]
 origin_city = sys.argv[2]
 destination_city = sys.argv[3]
-city_set = set()
+city_set = {}
+list = []
 
 print(input_filename, origin_city, destination_city)
 
@@ -14,12 +15,35 @@ with open(input_filename, "r") as file:
 
         for char in line:
             last_index += 1
+
             if char == ' ':
-                city_set.add(line[start_index:last_index])
-                start_index = last_index
+                current_index = last_index # Used for second loop to obtain second city name
+                for char2 in line[last_index:]:
+                    current_index += 1
+                    if char2 == ' ':
+
+                        if line[start_index:last_index-1] not in city_set:
+                            list.append([line[last_index:current_index-1], line[current_index:-1]])
+                            city_set.update({line[start_index:last_index-1] : list}) # Used to store first city's available paths to other cities and path distance
+                        elif line[start_index:last_index-1] in city_set:
+                            city_set.get(line[start_index:last_index-1]).append([line[last_index:current_index-1], line[current_index:-1]])
+
+                        if line[last_index:current_index-1] not in city_set:
+                            list.append([line[start_index:last_index-1], line[current_index:-1]])
+                            city_set.update({line[last_index:current_index-1] : list}) # Used to store second city's available paths to other cities and path distance
+                        elif line[last_index:current_index-1] in city_set:
+                            city_set.get(line[last_index:current_index-1]).append([line[start_index:last_index-1], line[current_index:-1]])
+
+                        list = [] # Resets list to 0
+                        break
+                break
+
+                # start_index = last_index
+            
             elif line[0:last_index] == "END":
                 break
-    print(city_set)
+
+    print(city_set.get("Bremen"))
         
 
 l = set()
